a890c7c7f05477315f6acfd20836f0dcf6bdb0b1
NoelHuibers <Noel_Huibers@web.de>
2021-11-15 08:33:01 +0100

    PhP for Working Contact Form

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $name = $_POST["name"];
+    $mailFrom = $_POST["email"];
+    $message = $_POST["message"];
+
+    $mailTo = "noel@huibers.io";
+    $subject = "Mail from my Website";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an e-mail from ".$name.".\nEmail address: ".$mailFrom."\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
+?>
