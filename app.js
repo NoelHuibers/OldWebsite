9809c518df36867934d389ed7e61332bd2f803a6
NoelHuibers <Noel@huibers.io>
2021-11-17 12:15:29 +0100

    added Animation

--- app.js
+++ app.js
@@ -33,3 +33,14 @@ window.onscroll = () => {
     }
   });
 };
+
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+var animation = lottieWeb.loadAnimation({
+  container: document.getElementById('containerSVG'),
+  path: 'data.json',
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  name: "Demo Animation",
+});
