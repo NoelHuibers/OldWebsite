aa4034c01ff39ef569a11cb9f1af767c4b1a15c2
NoelHuibers <Noel_Huibers@web.de>
2021-11-18 12:09:15 +0100

    Burger for Mobile

--- app.js
+++ app.js
@@ -33,3 +33,19 @@ window.onscroll = () => {
     }
   });
 };
+
+const primaryNav = document.querySelector(".primary-navbar")
+const navToggle = document.querySelector(".hamburger")
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility == "false"){
+    primaryNav.setAttribute("data-visible", true)
+    navToggle.setAttribute("aria-expanded", true)
+  }
+  else {
+    primaryNav.setAttribute("data-visible", false)
+    navToggle.setAttribute("aria-expanded", false)
+  }
+})
