50d79facb46718015120fcd71fc2bf5f3c34aa8f
NoelHuibers <Noel_Huibers@web.de>
2021-11-21 18:48:29 +0100

    Kebab changes and Fixes of dead code

--- app.js
+++ app.js
@@ -34,18 +34,18 @@ window.onscroll = () => {
   });
 };
 
-const primaryNav = document.querySelector(".primary-navbar")
-const navToggle = document.querySelector(".hamburger")
+const primaryNav = document.querySelector(".primary-navbar");
+const navToggle = document.querySelector(".hamburger");
 
 navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
 
   if (visibility == "false"){
-    primaryNav.setAttribute("data-visible", true)
-    navToggle.setAttribute("aria-expanded", true)
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
   }
   else {
-    primaryNav.setAttribute("data-visible", false)
-    navToggle.setAttribute("aria-expanded", false)
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
-})
+});
