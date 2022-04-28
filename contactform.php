3cc17174aa32ba80aed02f00149b092b124647a7
Noel Huibers <noel@huibers.io>
2022-04-28 21:36:19 +0200

    Honeypot

--- contactform.php
+++ contactform.php
@@ -1,16 +1,22 @@
 <?php
 
 if (isset($_POST["submit"])) {
-  $name = $_POST["name"];
-  $mailFrom = $_POST["email"];
-  $message = $_POST["message"];
-
-  $mailTo = "noel@huibers.io";
-  $subject = "Mail from my Website";
-  $headers = "From: ".$mailFrom;
-  $txt = "You have recieved an e-mail from ".$name.".\nEmail address: ".$mailFrom."\n\n".$message;
-
-  mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.html?mailsend");
+  $honeypot = FALSE;
+  if (!empty($_REQUEST['contact_me_by_fax_only']) && (bool) $_REQUEST['contact_me_by_fax_only'] == TRUE) {
+    $honeypot = TRUE;
+    log_spambot($_REQUEST);
+  } else {
+    $name = $_POST["name"];
+    $mailFrom = $_POST["email"];
+    $message = $_POST["message"];
+  
+    $mailTo = "noel@huibers.io";
+    $subject = "Mail from my Website";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an e-mail from ".$name.".\nEmail address: ".$mailFrom."\n\n".$message;
+  
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+  }
 }
 ?>
